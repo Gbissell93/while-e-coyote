fd654c1c3b0ae12d3137d1d5ab93eb330b70903c
Gbissell93 <gregorybisselledu@gmail.com>
2021-06-02 12:25:17 -0400

    number-summer update

--- number-summer.js
+++ number-summer.js
@@ -1,17 +1,27 @@
 const prompt = require("prompt-sync")();
 
 //USER INPUT
-const intro = prompt("please press done when finished")
-let sum = 0;
+const intro = prompt("please press 'done' when finished")
+
+// LOOP CONDITIONAL VARIABLES
+let count = 0;
  
 let add = true;
 
+let sum = 0;
+
+//LOOP START
 while (add == true) {
-    sum++;
-    const retry = prompt(`sum is ${sum}. go again?`);
+    count++
+    const retry = prompt(`Count is ${count}. Would you like to go again?`)
+    sum = sum + count;
+ //LOOP CONDITIONAL
     if (retry == "done") {
     add = false;
     console.log(`The sum is ${sum}.`)
     }
+
+   
+    
 }
 
